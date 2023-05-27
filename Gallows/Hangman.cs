b759a81e59c8ed1ce3e58ad24ab1b367875fa81c
CoderBrothers <121336523+CoderBrothers@users.noreply.github.com>
2023-05-27 11:00:19 +0300

    Add project files.

---
+++ Gallows/Hangman.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallows
+{
+    internal class Hangman
+    {
+        private Dictionary<string, string> words = new()
+    {
+        { "apple", "Description of apple" },
+        { "pineapple", "Description of pineapple" },
+        { "orange", "Description of orange" },
+        { "grapes", "Description of grapes" },
+        { "melon", "Description of melon" },
+    };
+        private string word;
+        private List<char> guessedLetters;
+        private int attempts = 6;
+        //private string 
+
+        public void Play()
+        {
+            word = GetRandomKey();
+            guessedLetters = new();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(word);
+                Console.WriteLine($"{words[word]}\nAttempts: {attempts}");
+                DrawWord();
+                Console.WriteLine("Enter a letter:");
+                char.TryParse(Console.ReadLine(), out char guess);
+                if (!char.IsLetter(guess))
+                {
+                    Console.WriteLine("Incorrect input. Try again.\nPress any key to continue.");
+                    Console.ReadKey();
+                    continue;
+                }
+                if (IsLetterAlreadyGuessed(guess))
+                {
+                    Console.WriteLine("You already guessed that letter. Try again.\nPress any key to continue.");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                guessedLetters.Add(guess);
+                if (word.Contains(guess))
+                {
+                    Console.WriteLine("Correct guess!");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong guess!");
+                    attempts--;
+                }
+
+                if (attempts == 0)
+                {
+                    Console.WriteLine("Game over! The word was: " + word);
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+        private string GetRandomKey()
+        {
+            var keys = words.Keys.ToList();
+            Random index = new Random();
+            return keys[index.Next(keys.Count)];
+        }
+        private void DrawWord()
+        {
+            foreach (var letter in word)
+            {
+                if (guessedLetters.Contains(letter))
+                {
+                    Console.Write(letter + " ");
+                }
+                else
+                {
+                    Console.Write("* ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private bool IsLetterAlreadyGuessed(char letter) => guessedLetters.Contains(letter);
+    }
+}
---
+++ Gallows/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Gallows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hangman hangman = new();
+            hangman.Play();
+        }
+    }
+}
